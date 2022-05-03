8932bb9ce44802aa841f3bc6ee44551014d489cb
Charles D. Webb-McClain <89601203+Alchenda@users.noreply.github.com>
2022-05-03 17:48:43 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <list>
 #include <pthread.h>
 #include "ThreadCreation.hpp"
 
@@ -20,13 +21,13 @@ int pageSize;
 typedef struct {
     int threadNum;
 } threadID;
-
+list<int> readyFrameList;
 int main(int argc, char * argv[]) {
     char *x = argv[1]; //input file
     char *y = argv[2]; //output file
     char *z = argv[3]; //seed for random number generation
     threadID args = {-5};
-    int seed, mainMemSize, numThreads;
+    int seed, mainMemSize, numThreads, freeFrameSize;
     string tempLoad;
     vector<string> threadFiles;
     fstream inFiles;
@@ -48,8 +49,12 @@ int main(int argc, char * argv[]) {
     mainMemSize = stoi(threadFiles.at(0));
     pageSize = stoi(threadFiles.at(1));
     numThreads = stoi(threadFiles.at(2));
+    //Establish free frame list and list size
+    freeFrameSize = mainMemSize / pageSize; //number of frames that are in physical memory
+    for (int i = 0; i < freeFrameSize; ++i) {
+        readyFrameList.push_back(i); //every frame starts off ready so populate with every frame
+    }
     //create threads and have them open their files
-    //threads = new pthread_t[numThreads];
     pthread_t newThread;
     for (int i = 3; i < threadFiles.size() - 1; ++i) {
         args = {i};
