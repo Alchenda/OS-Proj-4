837d89d1c94cdcbb82b272670fc198d795fd9dd8
Charles D. Webb-McClain <89601203+Alchenda@users.noreply.github.com>
2022-05-03 17:49:01 -0400

    Update ThreadCreation.cpp

--- ThreadCreation.cpp
+++ ThreadCreation.cpp
@@ -8,31 +8,46 @@
 #include "ThreadCreation.hpp"
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <math.h>
+#include "PTE.hpp"
 using namespace std;
 extern int const pageSize;
 extern pthread_mutex_t lock1;
 void* CreateThread(void * arg){
-    int* pageTable; // needs to be dynamically allocated since it changes based on size given
     string threadFileName = *(string*)arg;//type casting the void pointer
     fstream fileForThread;
     string line;
+    string readInput[10][3];
+    string input1, input2, input3;
+    int counter = 0;
     double offset;
     int virtualMemSize;
     int bitsForPageNum;
     int pageTableSize;
-    pthread_mutex_lock(&lock1);
-    cout << "This is the thread that is going to be read " << threadFileName << endl;
-    pthread_mutex_unlock(&lock1);
+    vector<PTE> pageTable;
+    //open the threads respectibe file
     fileForThread.open(threadFileName);
-    if (fileForThread.is_open()) {
+    /*if (fileForThread.is_open()) {
         if (getline(fileForThread, line)) {
             cout << line << endl;
         }
+    }*/
+    while (!fileForThread.eof()) {
+        if (counter == 0) {
+            fileForThread >> line;
+        } else {
+            fileForThread >> input1; //command
+            fileForThread >> input2; //register number
+            fileForThread >> input3; //virtual address
+            pageTable.push_back(PTE(input1, input2, input3));
+        }
+        counter++;
     }
     virtualMemSize = stoi(line);
     offset = log2(pageSize);
     bitsForPageNum = 32 - offset;
-    pageTableSize = pow(2, bitsForPageNum);
+    pageTableSize = virtualMemSize / pageSize;
+    //creating the page table for the process
     return nullptr;
 }
