bc311adf9243d0ae4cb8cd6a178a2c88166bb6af
Charles D. Webb-McClain <89601203+Alchenda@users.noreply.github.com>
2022-05-06 14:14:35 -0400

    Update PTE.cpp

--- PTE.cpp
+++ PTE.cpp
@@ -16,43 +16,21 @@ PTE::PTE(string command, string registerNum, string VA){
     RW = command;
     regNum = registerNum;
     std::stringstream ss;
+    VirtAdd = VA;
     temp = stoi(VA);
     pageNumber = temp / 32;
-    //IntToHex(temp);
+    referenceBit = 0;
+    validBit = 0;
+    
 }
-//not working I am not sure why
-void PTE::IntToHex(int convert){
-        // char array to store hexadecimal number
-        char hexaDeciNum[100];
-     
-        // counter for hexadecimal number array
-        int i = 0;
-        while (convert != 0) {
-        // temporary variable to store remainder
-        int temp = 0;
-     
-        // storing remainder in temp variable.
-        temp = convert % 16;
-     
-        // check if temp < 10
-        if (temp < 10) {
-            hexaDeciNum[i] = temp + 48;
-            i++;
-        }
-        else {
-            hexaDeciNum[i] = temp + 55;
-            i++;
-        }
-     
-        convert = convert / 16;
-    }
-     
-    // printing hexadecimal number array in reverse order
-    for (int j = i - 1; j >= 0; j--)
-            VirtAdd += hexaDeciNum[j];
+string PTE::GetCommand(){
+    return RW;
 }
-void PTE::PrintVA(){
-    cout << "current first page VA " << VirtAdd << endl;
+string PTE::GetRegister(){
+    return regNum;
+}
+string PTE::GetVA(){
+    return VirtAdd;
 }
 void PTE::PrintReg(){
     cout << regNum << endl;
